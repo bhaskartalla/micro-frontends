06fdabd64a716d036378db504c9ba77d504f3c72
bb7talla@gmail.com <bhaskar@bhaskars-MacBook-Air.local>
2025-06-16 20:19:40 +0530

    Initial Commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Hi there..!</h2>
+      <hr />
+      <MarketingApp />
+    </div>
+  )
+}
+
+export default App
