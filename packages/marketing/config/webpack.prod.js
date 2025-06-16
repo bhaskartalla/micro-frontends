0a3ab0182b4b5dc96db70070750d8054e960b545
bb7talla@gmail.com <bhaskar@bhaskars-MacBook-Air.local>
2025-06-16 20:49:57 +0530

    Creating production webpack config files

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
+
+const domain = process.env.PRODUCTION_DOMAIN
+
+const productionConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootStrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+}
+
+module.exports = merge(commonConfig, productionConfig)
