f0b46616e0e68f8cb7bd901e0b312fee21241d87
bb7talla@gmail.com <bhaskar@bhaskars-MacBook-Air.local>
2025-08-19 19:47:36 +0530

    Build issue fixed

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -10,7 +10,7 @@ const productionConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/marketing/latest/',
+    publicPath: '/auth/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
